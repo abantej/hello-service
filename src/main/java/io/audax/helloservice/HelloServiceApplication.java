9e00733b568455dd34fbc1d66db1283f72b75494
abantej <joeward.l.abante@gmail.com>
2023-10-13 08:12:38 +0800

    added tempo and loki

--- src/main/java/io/audax/helloservice/HelloServiceApplication.java
+++ src/main/java/io/audax/helloservice/HelloServiceApplication.java
@@ -1,7 +1,16 @@
 package io.audax.helloservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class HelloServiceApplication {
@@ -10,4 +19,39 @@ public class HelloServiceApplication {
 		SpringApplication.run(HelloServiceApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@RestController
+	class HelloController {
+
+		private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+		private final RestTemplate restTemplate;
+
+		HelloController(RestTemplate restTemplate) {
+			this.restTemplate = restTemplate;
+		}
+
+		@GetMapping("/hello")
+		public String hello() {
+			LOGGER.info("--------Hello method started--------");
+			LOGGER.error("--------Hello method started, missing id--------");
+			ResponseEntity<String> responseEntity = this.restTemplate.getForEntity("http://localhost:8081/hi",
+					String.class);
+			return responseEntity.getBody();
+		}
+
+		@GetMapping("/exception")
+		public String exception() {
+			throw new IllegalArgumentException("This id is invalid");
+		}
+
+		@ExceptionHandler(value = { IllegalArgumentException.class })
+		protected ResponseEntity<String> handleConflict(IllegalArgumentException ex) {
+			LOGGER.error(ex.getMessage());
+			return ResponseEntity.badRequest().body(ex.getMessage());
+		}
+	}
 }
